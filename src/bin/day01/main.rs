44fa05b98e6332819861fc3bf270678ea17f3415
Burhan Keten <ketenburhan@protonmail.com>
2023-12-02 18:25:38 +0300

    day01_part2

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 const DATA: &str = include_str!("data.txt");
 
 fn main() {
@@ -15,23 +17,58 @@ fn part1(data: &str) -> u32 {
         .sum()
 }
 
+const REGEX_TEXT: &str = r"one|two|three|four|five|six|seven|eight|nine|\d";
+
 fn part2(data: &str) -> usize {
-    todo!()
+    data.lines()
+        .map(|line| {
+            let re = Regex::new(REGEX_TEXT).unwrap();
+
+            let first = re.find(line).unwrap().as_str();
+            let first = decode(first);
+
+            let last = (0..line.len())
+                .rev()
+                // .inspect(|i| println!("debug: {i}"))
+                .find_map(|i| re.find_at(line, i))
+                .unwrap()
+                .as_str();
+            let last = decode(last);
+
+            first * 10 + last
+        })
+        .sum()
+}
+
+fn decode(text: &str) -> usize {
+    match text {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        x => x.parse::<usize>().unwrap(),
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const DATA: &str = include_str!("example.txt");
+    const DATA1: &str = include_str!("example1.txt");
+    const DATA2: &str = include_str!("example2.txt");
 
     #[test]
     fn part1_test() {
-        assert_eq!(part1(DATA), 142);
+        assert_eq!(part1(DATA1), 142);
     }
 
     #[test]
     fn part2_test() {
-        assert_eq!(part2(DATA), 45000);
+        assert_eq!(part2(DATA2), 281);
     }
 }
